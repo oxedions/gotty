f3805d53b244370c29ab515d148382f47538a734
oxedions <oxedions@gmail.com>
2022-07-06 13:43:19 +0200

    Revert autoclose

--- server/handlers.go
+++ server/handlers.go
@@ -46,7 +46,7 @@ func (server *Server) generateHandleWS(ctx context.Context, cancel context.Cance
 				closeReason, r.RemoteAddr, num, server.options.MaxConnection,
 			)
 
-			if (server.options.Once) || (closeReason == "local command") {
+			if server.options.Once {
 				cancel()
 			}
 		}()
